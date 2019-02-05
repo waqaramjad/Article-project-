be6b82c620141cf3b6346cad2f9359abbab8cd4a
waqaramjad <waqaramjad345@gmail.com>
2019-02-05 15:32:13 +0500

    code work  perfectly

--- components/LoginSignup/Controls/index.js
+++
@@ -1,9 +0,0 @@
-import _ButtonRound from './ButtonRound'
-import _ButtonRoundBlue from './ButtonRoundBlue'
-import _IconImage from './IconImage'
-import _IconInput from './IconInput'
-
-export const ButtonRound = _ButtonRound
-export const ButtonRoundBlue = _ButtonRoundBlue
-export const IconImage = _IconImage
-export const IconInput = _IconInput
